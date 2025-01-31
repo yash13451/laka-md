5f94bb8f30d63f4ec9f487189db1b7fe9c8b199a
© ᴍʀ-ʟᴀᴋᴀ 🧸🖇️✨ <lakaofc@gmail.com>
2025-01-31 14:55:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,6 +8,12 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 //👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
 SESSION_ID: process.env.SESSION_ID || "Put Your Session Id 🐣",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "𝐘𝐨𝐮𝐫 𝐒𝐭𝐚𝐭𝐮𝐬 𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐜𝐥𝐲 𝐒𝐞𝐞𝐧𝐬 𝐁𝐲 𝐋𝐚𝐤𝐚-𝐌𝐝 🧚‍♀️🐣💗",
+
+
+//=================================================
+
+
 
 PREFIX: process.env.PREFIX || '.' ,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94781121579' : process.env.OWNER_NUMBER,
